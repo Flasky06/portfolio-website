ff398c1a6e7d3f647ed2b95004cd6fbecd166d5b
Flasky06 <bonnienjuguna106@gmail.com>
2023-03-06 20:27:44 +0300

    details commit

--- portfolio-nextjs/components/About.jsx
+++ portfolio-nextjs/components/About.jsx
@@ -23,12 +23,10 @@ const About = () => {
 						specific language, but choosing the best tool for the job.
 					</p>
 					<p className="py-2 text-gray-600">
-						I started web developement in 2021 managing multiple e-commerce
-						websites on CMS platforms such as WordPress, BigCommerce, and
-						Shopify. I have experience working directly with clients and taking
-						mock wireframes all the way to deployed applications. In my spare
-						time I run Code Commerce, a Youtube channel where I teach web
-						developement and various front-end technologies.
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. In eum,
+						est incidunt nesciunt soluta tempora temporibus possimus laborum
+						iusto dolore. Laborum eius consectetur repudiandae et ab voluptate
+						ullam recusandae iure?
 					</p>
 					<Link href="/#projects">
 						<p className="py-2 text-gray-600 underline cursor-pointer">
